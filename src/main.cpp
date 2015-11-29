7261011489bfb936592dada8f2ea1dd26aba3cd6
Jeffrey Burton <burton.jeff.r@gmail.com>
2015-11-28 23:04:01 -0800

    updated main.cpp to run through all strings in strings.txt. Added more words to strings.txt --note: some words were spelled wrong on purpose!

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,27 @@
 #include "./headers/LevenshteinCheckerRecursive.h"
 #include "./headers/LevenshteinCheckerDynamic.h"
 
+
 main() {
-  auto f = new FileReaderStack("./config/strings.txt");
+  auto f1 = new FileReaderStack("./config/strings.txt");
+  auto f2 = new FileReaderStack("./config/strings.txt");
   auto dict = new Dictionary("./config/dictionary.txt");
-  f->readFile();
-  auto checker = new LevenshteinCheckerDynamic(dict, f);
+  f1->readFile();
+  f2->readFile();
+  auto checkerD = new LevenshteinCheckerDynamic(dict, f1);
+  auto checkerR = new LevenshteinCheckerRecursive(dict, f2);
 
   std::cout << "init finished" << std::endl;
 
-  checker->checkAgainstDictionary(); //returns empty string...
-  checker->checkAgainstDictionary();
-  checker->checkAgainstDictionary();
-  checker->checkAgainstDictionary();
+  //remove empty strings
+  checkerD->checkAgainstDictionary();
+  checkerR->checkAgainstDictionary();
+
+
+  for (int i = 0; i < f1->getUnparsedStringSize(); i++) {
+    checkerD->checkAgainstDictionary();
+  }
+
 
   std::cout << std::endl;
   return 0;
